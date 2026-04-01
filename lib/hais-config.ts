03ca5422b76db72520202ebabf910dadf98e60dc
Hideki Tamae <tamatixyan@gmail.com>
2026-04-01 18:03:13 +0900

    Fix hais-config.ts syntax error

--- lib/hais-config.ts
+++ lib/hais-config.ts
@@ -1,4 +1,4 @@
-haisexport const HAIS_CONFIG = {
+export const HAIS_CONFIG = {
     title: 'HAIS - 医療機関向けプレミアム分析システム',
     securityNotice: '【重要】本システムは医療関係者専用の高度セキュア領域です。無断アクセス及びデータの複製を固く禁じます。',
 };
